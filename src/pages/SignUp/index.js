c9b615dea1e3fb90b715a7a66fc7a60e6f3babf0
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-03-16 15:55:53 -0300

    FE-CLIENT / Estiliza as paginas de login e cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,22 @@
-/* --------------------------------- IMPORTS ---------------------------------*/
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/"> Já estou cadastrado </Link>
+      </form>
+    </>
+  );
 }
